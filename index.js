bba0136a53b370ea035b954507d83d802c0063ac
Gabriel Rak <grakzanchetta@gmail.com>
2022-07-03 21:15:14 -0300

    login feito

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import joi from 'joi';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
+import {v4 as uuid} from 'uuid';
 
 import db from './db.js';
 
@@ -13,6 +14,33 @@ app.use(cors());
 dotenv.config();
 
 const port = process.env.PORT || 5000
+
+app.post("/signin", async (request, response) => {
+
+  const signInSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required()
+  });
+  const { error } = signInSchema.validate(request.body, {abortEarly: false});
+    if(error){
+        return response.sendStatus(422)
+    }
+
+  try {
+    const user = await db.collection("users").findOne({email: request.body.email})
+    if(!user) return response.sendStatus(404);
+    if(user && bcrypt.compareSync(request.body.password, user.password)){
+      const token = uuid();
+      await db.collection("sessions").insertOne({token, userId: user._id})
+      response.send({token, name: user.name});
+    } else {
+      return response.sendStatus(401)
+    }
+  } catch {
+    return response.sendStatus(500);
+  }
+});
+
 
 app.post("/signup", async (request, response) => {
     const {name, email, password, confirmPassword} = request.body
